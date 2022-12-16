f4e8b81fee94d633c63ea31545cc91f4fe544098
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2022-12-16 18:12:15 +0200

    Create terms.js

---
+++ src/pages/terms.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Terms Of Use</h1>
+        <p>
+          This is an example site integrating Netlify’s form handling with Gatway
+        </p>
+
+        <ul>
+          <li><Link to="/contact/">Contact</Link></li>
+          <li><Link to="/file-upload/">Sign Up</Link></li>
+          <li><Link to="/recaptcha/">Policy</Link></li>
+        </ul>
+      </div>
+    );
+  }
+}
