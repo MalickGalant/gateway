fdb27e1de5ece1c7bb7d5447a74427f9e37abfd9
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2022-12-16 20:03:12 +0200

    Create fancy-link.js

---
+++ src/components/fancy-link.js
@@ -0,0 +1,4 @@
+import Readme from './readme.md' // Assumes an integration is used to compile MDX -> JS.
+import {FancyLink} from './components/fancy-link.js'
+
+<Readme components={{a: FancyLink}} />
