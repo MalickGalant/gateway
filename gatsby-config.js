aa0064ff960036e3de6fa8992c482b3eac18bbdc
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2022-12-16 19:49:10 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,6 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Netlify Form Integration`
   },
-  plugins: [`gatsby-plugin-react-helmet`]
+  plugins: [`gatsby-plugin-react-helmet`] ,
+  plugins: ['gatsby-plugin-theme-ui'],
 };
