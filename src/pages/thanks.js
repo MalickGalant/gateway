48281f75f0ed28434dc8c1b3c5fda29758c0a65c
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2022-12-16 17:54:54 +0200

    Update thanks.js

--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -2,7 +2,8 @@ import React from "react";
 
 export default () => (
   <div>
-    <h1>Thank you!</h1>
+    <h1>Thank You!</h1>
     <p>This is a custom thank you page for form submissions</p>
+
   </div>
 );
