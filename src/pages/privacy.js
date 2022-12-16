abf51082a4fc8fba6c0bed6df72dfb78286ffb57
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2022-12-16 18:11:23 +0200

    Create privacy.js

---
+++ src/pages/privacy.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Link from "gatsby-link";
+import Helmet from "react-helmet";
+
+export default class Index extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Privacy Policy</h1>
+        <p>
+          This is an example site integrating Netlify’s form handling with Gatway
+        </p>
+
+        <ul>
+          <li><Link to="/contact/">Contact</Link></li>
+          <li><Link to="/file-upload/">Sign Up</Link></li>
+          <li><Link to="/terms/">TermsOf Use</Link></li>
+        </ul>
+      </div>
+    );
+  }
+}
